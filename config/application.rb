266e53e3f2f1013a633646b00d2dd16b45b183f0
nagireddy <chanti.mule123@gmail.com>
2017-08-26 13:18:53 +0530

    editing form with horizantal

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,11 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Start
   class Application < Rails::Application
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822180745) do
+ActiveRecord::Schema.define(version: 20170823063906) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
